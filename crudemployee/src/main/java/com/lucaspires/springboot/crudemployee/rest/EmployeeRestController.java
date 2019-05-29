52ed2da95bbc2dae35245003860eeddd54704e33
Lucas Pires <lucas_pires@outlook.com>
2019-05-29 15:15:39 -0300

    New feature: update an existing employee

--- crudemployee/src/main/java/com/lucaspires/springboot/crudemployee/rest/EmployeeRestController.java
+++ crudemployee/src/main/java/com/lucaspires/springboot/crudemployee/rest/EmployeeRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +30,7 @@ public class EmployeeRestController {
 	}
 	
 	@GetMapping("/employees/{employeeId}")
-	public Employee findById(@PathVariable int employeeId) {
+	public Employee getEmployee(@PathVariable int employeeId) {
 		Employee theEmployee = employeeService.findById(employeeId);
 		
 		if (theEmployee == null) {
@@ -40,10 +41,17 @@ public class EmployeeRestController {
 	}
 	
 	@PostMapping("/employees")
-	public Employee save(@RequestBody Employee theEmployee) {
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
 		// if one pass an id in JSON, set it to 0 (forcing to save a new object, instead of update)
 		theEmployee.setId(0);
 		
+		employeeService.save(theEmployee);
+		
+		return theEmployee;
+	}
+	
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee theEmployee) {
 		employeeService.save(theEmployee);
 		
 		return theEmployee;
